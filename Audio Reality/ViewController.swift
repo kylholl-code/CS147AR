00b84c5996b22b19ee712965d69b517112785525
Kylie Holland <kylholl@stanford.edu>
2019-11-11 18:54:11 -0800

    Initial Commit

---
+++ Audio Reality/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Audio Reality
+//
+//  Created by Kylie Holland on 11/11/19.
+//  Copyright © 2019 audioreality. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
